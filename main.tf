d49e86b3a9bccb25484974406728a66e416fd739
Heera <71284609+Heerananthini@users.noreply.github.com>
2022-08-10 09:29:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,3 @@ resource "aws_instance" "web" {
      "Name" = "web"
   }
 }
-module "instance-practice" {
-  source  = "app.terraform.io/git_practice_ec2/instance-practice/aws"
-  version = "1.0.0"
-  # insert required variables here
-  ami = var.ami
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.web_subnet.id
-}
